8db3b0fe9b68ed1cc5042a1c2fe5768f0c1cc8e0
osamamhd <osamamhd82@gmail.com>
2021-07-16 04:31:46 +0200

    🔥 models and admin register.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Article, Category, Comment
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'tag_list', 'get_read_time']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['text', 'author', 'created_at', 'article']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
--- main/models.py
+++ main/models.py
@@ -1,3 +1,92 @@
+from django.core.files import File
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from io import BytesIO
+from PIL import Image
+
+from autoslug import AutoSlugField
+from ai_django_core.models import CommonInfo
+from taggit.managers import TaggableManager
+
+localhost = 'http://127.0.0.1:8000/'
+STATUS_CHOICES = [('Published', 'PUBLISHED'), ('Drafted', 'DRAFTED')]
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, db_index=True)
+    slug = AutoSlugField(populate_from='title', unique_with='description')
+    description = models.CharField(max_length=300, blank=True)
+    cover = models.ImageField(upload_to='uploads/categories/', blank=True, null=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+
+
+class Article(CommonInfo, models.Model):
+    title = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title', unique_with='created_at')
+    description = models.CharField(max_length=300, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(upload_to='uploads/covers', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/thumbnails/', blank=True, null=True)
+    content = models.TextField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=15, default='Drafted')
+    like = models.PositiveSmallIntegerField(default=0)
+    heart = models.PositiveSmallIntegerField(default=0)
+    happy = models.PositiveSmallIntegerField(default=0)
+    tags = TaggableManager()
+
+    def get_absolute_url(self):
+        return f'{self.category.slug}/{self.slug}/'
+
+    def get_read_time(self):
+        return len(self.content) // 250
+
+    def get_image(self):
+        if self.image:
+            return localhost + self.image.url
+        return ''
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return localhost + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return localhost + self.thumbnail.url
+            else:
+                return ''
+
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=50)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name="comments")
+    text = models.TextField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:50]
