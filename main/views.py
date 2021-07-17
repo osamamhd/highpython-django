cc2f0bacb785c4ecef0660acfc85c28d811dbe71
osamamhd <osamamhd82@gmail.com>
2021-07-17 03:16:54 +0200

    👽️ article list view and url.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .serializers import ArticleSerializer
+from .models import Article
 
-# Create your views here.
+
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
