9f3d6dc9651e67325be5a3b5f879c8e4d7cd3102
osamamhd <osamamhd82@gmail.com>
2021-07-17 19:23:32 +0200

    📄 add urls for article, categories and comments.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    CategoriesListView,
+    CategoryDetailView,
+    CommentCreateListAPIView,
+
+    )
 
 urlpatterns = [
-    path('', ArticleListView.as_view())
+    path('', ArticleListView.as_view()),
+    path('<slug:category_slug>/<slug:article_slug>/', ArticleDetailView.as_view()),
+    path('categories/', CategoriesListView.as_view()),
+    path('<slug:category_slug>/', CategoryDetailView.as_view()),
+    path('<int:article_pk>/comments/', CommentCreateListAPIView.as_view()),
 ]
