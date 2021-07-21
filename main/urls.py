23661d0f88212357383eab73512609d4f9f3360c
osamamhd <osamamhd82@gmail.com>
2021-07-21 02:44:00 +0200

    ✨ urls for heart, like and happy.

--- main/urls.py
+++ main/urls.py
@@ -5,13 +5,18 @@ from .views import (
     CategoriesListView,
     CategoryDetailView,
     CommentCreateListAPIView,
-
+    article_heart,
+    article_happy,
+    article_like,
     )
 
 urlpatterns = [
+    path('<int:article_pk>/comments/', CommentCreateListAPIView.as_view()),
+    path('<int:pk>/heart/', article_heart),
+    path('<int:pk>/happy/', article_happy),
+    path('<int:pk>/like/', article_like),
     path('', ArticleListView.as_view()),
     path('<slug:category_slug>/<slug:article_slug>/', ArticleDetailView.as_view()),
     path('categories/', CategoriesListView.as_view()),
     path('<slug:category_slug>/', CategoryDetailView.as_view()),
-    path('<int:article_pk>/comments/', CommentCreateListAPIView.as_view()),
 ]
